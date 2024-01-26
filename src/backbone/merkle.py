4fc621adb4f8ba9476b6c4612ec2cad06b2d1874
Jurian Onderwater <jurian.onderwater247@gmail.com>
2024-01-26 12:15:12 +0100

    merkle leeaves set to transactions

--- src/backbone/merkle.py
+++ src/backbone/merkle.py
@@ -3,10 +3,11 @@
 class MerkleTree:
     def __init__(self, txs):
         self.data = None
-        self.leaf_nodes = None
+        self.leaf_nodes = txs
         self.build_tree()
 
     def build_tree(self):
+        # hash here already or just build tree and build in POW
         return None
 
     def get_root(self):
