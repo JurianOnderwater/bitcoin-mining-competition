8f2cd93018e143195912e8dc94d8883b38a01184
Jurian Onderwater <jurian.onderwater247@gmail.com>
2024-01-27 13:13:45 +0100

    Merkle tree start

--- src/backbone/merkle.py
+++ src/backbone/merkle.py
@@ -1,4 +1,13 @@
-# TODO: Make Merkle tree structure
+class Node:
+    def __init__(self, left, right, transaction, hashed_value) -> None:
+        self.left = left
+        self.right = right
+        self.transaction = transaction
+        self.hashed_value = hashed_value
+
+    def hash(self, value):
+        # return someHashFunction(value)
+        return hash(value) #Standard hash function in python, didnt read up on what exact function to use
 
 class MerkleTree:
     def __init__(self, txs):
@@ -7,8 +16,22 @@ class MerkleTree:
         self.build_tree()
 
     def build_tree(self):
-        # hash here already or just build tree and build in POW
-        return None
+        # hash here already or just build tree and build in POW?
+        leaves: list[Node] = [Node(None, None, transaction, Node.hash(transaction)) for transaction in self.leaf_nodes]
+        self.tree = recursion(leaves)
+        def recursion(nodes):
+            middle = len(nodes)//2
+            if len(nodes) == 2:
+                f"{nodes[0].transaction}+{nodes[1].transaction}"
+                hashed_value = Node.hash(nodes[0].hashed_value + nodes[1].hashed_value)
+                return Node(nodes[0], nodes[1], transaction, hashed_value)
+        
+            left = recursion(nodes[:middle])
+            right = recursion(nodes[middle:])
+            transaction = f"{left.transaction}+{right.transaction}"
+            hashed_value = Node.hash(left.hashed_value + right.hashed_value)
+            return Node(left, right, transaction, hashed_value)
+        return self.tree
 
     def get_root(self):
-        return None
+        raise NotImplementedError("Not sure what to do here :(")
