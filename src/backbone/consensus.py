bb1524de1aa775c4f568f52df4b0aa6072172564
Jurian Onderwater <jurian.onderwater247@gmail.com>
2024-01-29 14:45:02 +0100

    consensus.py update

--- src/backbone/consensus.py
+++ src/backbone/consensus.py
@@ -1,27 +1,32 @@
 # backbone/consensus.py
+from abstractions.block import Block
 from merkle import MerkleTree
 from hashlib import sha256
+from server import DIFFICULTY
 
 
 # TODO: Implement Proof of Work
 class PoW:
-    def __init__(self) -> None:
+    def __init__(self, block: Block) -> None:
+        self.block = block
         self.proof()
 
+    def is_valid(self, value):
+        return value[:DIFFICULTY] == '0' * DIFFICULTY
+    
     def double_hash(value):
         if type(value) == str:
             return sha256(sha256(value))
         elif type(value) == list[str]:
             return sha256(sha256(str(''.join(value))))
 
-    def block_header(self, prev, timestamp, merkle):
-        return ''.join([str(prev), str(timestamp), str(merkle)])
+    def block_header(self, merkle: MerkleTree):
+        return ''.join([str(self.block.time), str(self.block.previous_block), str(merkle.get_root())])
 
     def proof(self):
-        NONCE = 0
-        while not IS_VALID(result):
-            result = self.double_hash([self.block_header(ATTRIBUTES), NONCE])
-            NONCE += 1
+        NONCE = self.block.nonce
+        while not self.is_valid(result):
+            result = self.double_hash([self.block_header(merkle=self.block.merkle_root), NONCE])
         BROADCAST_TO_NETWORK(result)
     """
     +----------------+
