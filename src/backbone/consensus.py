dea796aedcd158ab7cfc48365c26a20ca697b00e
Birk Lewin <birk.lewin@xait.com>
2024-01-31 14:55:48 +0100

    feat (consensus): Add sign method to PoW

--- src/backbone/consensus.py
+++ src/backbone/consensus.py
@@ -2,15 +2,27 @@
 from abstractions.block import Block
 from merkle import MerkleTree
 from hashlib import sha256
-from server import DIFFICULTY
+from server import DIFFICULTY, KEY_PAIRS_PATH, SELF
 import datetime
-
+import rsa
+from utils.cryptographic import load_private
 
 # TODO: Implement Proof of Work
 class PoW:
     def __init__(self, block: Block) -> None:
         self.block = block
         self.proof()
+
+    def sign(self, message):
+        """
+        sign with private key
+        :param message:
+        :return:
+        """
+        b_msg = bytes(message, 'utf-8')
+        with open(f"{KEY_PAIRS_PATH}/{SELF}_pbk.pem", "rb") as keyfile:
+            key = load_private(keyfile)
+            return rsa.sign(b_msg, key, 'SHA-1')
 
     def is_valid(self, value):
         return value[:DIFFICULTY] == '0' * DIFFICULTY
@@ -58,4 +70,3 @@ class PoW:
                 #   confirmed=False, 
                   mined_by=None, #Us
                   signature=None) #TODO
-
